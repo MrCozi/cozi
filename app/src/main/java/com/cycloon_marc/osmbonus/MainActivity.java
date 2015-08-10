5486a1588c8e91c808c6f4562fedf4b803c4f7b9
MrCozi <marccozijnsen@gmail.com>
2015-08-10 20:44:35 +0200

    fixed map centering issue fixed issue with incorrect turn by turn picture Changed from car navitagtion to bycicle navigation. changed languages settings to NL

--- app/src/main/java/com/cycloon_marc/osmbonus/MainActivity.java
+++ app/src/main/java/com/cycloon_marc/osmbonus/MainActivity.java
@@ -1,15 +1,17 @@
 package com.cycloon_marc.osmbonus;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewTreeObserver;
 
 import org.osmdroid.api.IMapController;
@@ -22,6 +24,7 @@ import org.osmdroid.bonuspack.routing.RoadNode;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
 
@@ -30,6 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private IMapController mapController;
 
     LocationManager locationManager;
+    ArrayList<GeoPoint> waypoints = new ArrayList<>();
+    ArrayList<OverlayItem> overlayItemArray;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +52,14 @@ public class MainActivity extends AppCompatActivity {
         mapController.setZoom(14);
         //mapController.setCenter(startPoint);
         ViewTreeObserver vto = map.getViewTreeObserver();
+
+        //set location
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if(lastLocation != null){
+            updateLoc(lastLocation);
+        }
+
         vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
@@ -67,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         startMarker.setTitle("Start point");
 
         map.getOverlays().add(startMarker);
-       map.invalidate();
+        map.invalidate();
 
         new Thread(new Runnable() {
             public void run() {
@@ -77,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 RoadManager roadManager = new MapQuestRoadManager("ji4nVIWGbEL8FANiCYM0JNGet7Fj9O8B");
                 roadManager.addRequestOption("routeType=bicycle");
                 roadManager.addRequestOption("locale=nl_NL");
-                ArrayList<GeoPoint> waypoints = new ArrayList<>();
+                //final ArrayList<GeoPoint> waypoints = new ArrayList<>();
                 waypoints.add(startPoint);
                 GeoPoint endPoint = new GeoPoint(52.976009, 6.558900);
                 waypoints.add(endPoint);
@@ -127,30 +141,85 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }).start();
+    }
+    protected void onResume() {
+            // TODO Auto-generated method stub
+            super.onResume();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, myLocationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, myLocationListener);
+        }
+    private void updateLoc(Location loc){
+        GeoPoint locGeoPoint = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+        mapController.setCenter(locGeoPoint);
 
+        setOverlayLoc(loc);
+
+        map.invalidate();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void setOverlayLoc(Location overlayloc){
+        //GeoPoint overlocGeoPoint = new GeoPoint(overlayloc);
+        //---
+       // overlayItemArray.clear();
+
+      //  OverlayItem newMyLocationItem = new OverlayItem(
+      //          "My Location", "My Location", overlocGeoPoint);
+      //overlayItemArray.add(newMyLocationItem);
+
+      // TEST CODE
+       GeoPoint beginPoint = new GeoPoint(overlayloc);
+        waypoints.add(beginPoint);
+
+        Marker beginMarker = new Marker(map);
+        map.getOverlays().remove(beginMarker);
+        beginMarker.setPosition(beginPoint);
+        beginMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+        beginMarker.setIcon(getResources().getDrawable(R.drawable.ic_menu_mylocation));
+        beginMarker.setTitle("Begin point");
+        map.getOverlays().add(beginMarker);
+        mapController.setCenter(beginPoint);
+        map.invalidate();
+
+
+        //---
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private LocationListener myLocationListener
+            = new LocationListener() {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public void onLocationChanged(Location location) {
+            // TODO Auto-generated method stub
+        //    updateLoc(location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // TODO Auto-generated method stub
+
+        }
+
+
+       // @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
 
 
+    };
 }
